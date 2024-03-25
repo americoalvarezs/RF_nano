04f1e0fa820bf806f45a77bb63f12c3ee0a65c82
americoalvarezs <40524057+americoalvarezs@users.noreply.github.com>
2024-03-24 22:06:29 -0400

    RECEPTOR.ino

--- RECEPTOR.ino
+++ RECEPTOR.ino
@@ -6,7 +6,7 @@
 Nrf24l Mirf = Nrf24l(10, 9);
 
 int value;
-int operacion;
+int operation;
 
 void setup()
 {
@@ -14,20 +14,22 @@ void setup()
   Mirf.spi = &MirfHardwareSpi;
   Mirf.init();
 
-  Mirf.setRADDR((byte *)"FGHIJ"); //Set your own address (receiver address) using 5 characters
+  Mirf.setRADDR((byte *)"FGHIJ"); //configura la direccion del RECEPTOR (direccion propia) en el ejemplo FGHIJ
   Mirf.payload = sizeof(value);
-  Mirf.channel = 90;             //Set the used channel
+  Mirf.channel = 90;             //canal utilizado
   Mirf.config();
-  Serial.println("Listening...");  //Start listening to received data
+  Serial.println("Listening...");  //empieza a escuchar datos si es que llega alguno...
 }
 
 void loop()
 {
-  if (Mirf.dataReady()) { //When the program is received, the received data is output from the serial port
+  if (Mirf.dataReady()) { // si hay datos en la pila de entrada entra al IF
     Mirf.getData((byte *) &value);
-    Serial.print("Got data: ");
+    
+    Serial.print("Got data: "); //Cuando se recibe dato, este se imprimen en el monitor serial por ejemplo
     Serial.println(value);
-    operacion=(value+7)*2;
+    
+    operacion=(value+7)*2;  //el valor en este caso es numerico y se puede realizar operaciones matematicas
     Serial.println(operacion);
   }
 }
