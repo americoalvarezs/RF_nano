b3c43347018fb9c0a80b58b4a15ac31edad71ece
americoalvarezs <40524057+americoalvarezs@users.noreply.github.com>
2024-03-24 22:02:33 -0400

    EMISOR.ino

--- EMISOR.ino
+++ EMISOR.ino
@@ -1,10 +1,9 @@
-//Transmitter program
-
 #include <SPI.h>
 #include "Mirf.h"
 #include "nRF24L01.h"
 #include "MirfHardwareSpiDriver.h"
 Nrf24l Mirf = Nrf24l(10, 9);
+
 int value=0;
 
 void setup()
@@ -12,24 +11,26 @@ void setup()
   Serial.begin(9600);
   Mirf.spi = &MirfHardwareSpi;
   Mirf.init();
-  //Set your own address (sender address) using 5 characters
+  //Establece tu propia dirección (dirección del EMISOR) usando 5 caracteres, en el ejemplo ABCDE
   Mirf.setRADDR((byte *)"ABCDE");
   Mirf.payload = sizeof(value);
-  Mirf.channel = 90;              //Set the channel used
+  Mirf.channel = 90;              //canal utilizado
   Mirf.config();
 }
 
 void loop()
 {
-  Mirf.setTADDR((byte *)"FGHIJ");           //Set the receiver address
+  Mirf.setTADDR((byte *)"FGHIJ");           //direccion del emisor FGHIJ, esto depende del RECEPTOR
+  
   if(value>50){
     value=0;
   }
-  Mirf.send((byte *)&value);                //Send instructions, send random number value
+  
+  Mirf.send((byte *)&value);                //enviar valor numerico (cuenta del 0 al 50)
   Serial.print("Wait for sending.....");
-  while (Mirf.isSending()) delay(1);        //Until you send successfully, exit the loop
+  while (Mirf.isSending()) delay(1);        //Se queda en el bucle hasta que se envie el dato correctamente, no importa si lo recibe o no el RECEPTOR
   Serial.print("Send success:");
   Serial.println(value);
   delay(1000);
-  value=value+1;
+  value=value+1;                            //incrementa la cuenta de 1 en 1
 }
